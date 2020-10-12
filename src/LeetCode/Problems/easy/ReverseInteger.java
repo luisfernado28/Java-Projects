dc0001fb0e9903d9f6f4a580043b1c0273684727
luisfernado28 <luisfer_2810@outlook.com>
2020-10-11 22:55:40 -0400

    Reverse integer solved

---
+++ src/LeetCode/Problems/easy/ReverseInteger.java
@@ -0,0 +1,25 @@
+package LeetCode.Problems.easy;
+
+public class ReverseInteger {
+    public static void main(String[] args){
+
+        System.out.println(reverse(1534236469));
+
+    }
+
+    public static int reverse(int x) {
+        int res=x%10;
+        x/=10;
+
+        while (x!=0){
+            int newNum=x%10;
+            x/=10;
+            res = Math.addExact(newNum , (res*10));
+
+       }
+       return res;
+
+
+
+    }
+}
