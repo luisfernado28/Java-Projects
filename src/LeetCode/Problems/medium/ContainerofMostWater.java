8c0aeb45fc2171afc5a8a529ea1a438ec5734964
luisfernado28 <luisfer_2810@outlook.com>
2020-10-23 23:31:32 -0400

    Container completed

--- src/LeetCode/Problems/medium/ContainerofMostWater.java
+++ src/LeetCode/Problems/medium/ContainerofMostWater.java
@@ -4,13 +4,13 @@ public class ContainerofMostWater {
 
     public static void main(String[] arg0){
 
-        System.out.println(containerOfWater(new int[]{4,3,2,1,4}));
+        System.out.println(containerOfWater(new int[]{1,8,6,2,5,4,8,3,7}));
     }
 
     public static int containerOfWater(int[] points){
-        int max=0, pointeri=0,pointerf=points.length;
+        int max=0, pointeri=0,pointerf=points.length-1;
         while (pointeri< pointerf){
-            int newCap= Math.abs(points[pointerf]-points[pointeri]) * (pointerf-pointeri);
+            int newCap= Math.min(points[pointerf],points[pointeri]) * (pointerf-pointeri);
             max= Math.max(max,newCap);
             if(points[pointeri]<=points[pointerf])
                 pointeri++;
